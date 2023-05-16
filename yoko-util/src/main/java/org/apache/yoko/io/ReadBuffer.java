1a3277d0594f1fe0325eff0e53c02d2a20b6b363
habib <habiblawal@hotmail.co.uk>
2023-05-16 16:21:46 +0100

    DOC: Updated any files which have copyright date ranges to have one date

--- yoko-util/src/main/java/org/apache/yoko/io/ReadBuffer.java
+++ yoko-util/src/main/java/org/apache/yoko/io/ReadBuffer.java
@@ -1,10 +1,10 @@
 /*==============================================================================
- * Copyright 2022, 2023 IBM Corporation and others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2023 IBM Corporation and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the License);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *=============================================================================*/
-
 package org.apache.yoko.io;
 
 import org.apache.yoko.util.HexConverter;
